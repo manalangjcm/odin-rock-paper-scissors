b127477d1d417b071b234bb576d3508cbe96a313
manalangjcm <159515420+manalangjcm@users.noreply.github.com>
2025-03-05 19:03:40 +0800

    feat: implement random computer choice

---
+++ main.js
@@ -0,0 +1,27 @@
+function getComputerChoice() {
+    // Create variable 'randomNumber' and store random numbers from 0-2
+    let randomNumber = parseInt(Math.floor(Math.random() * 3));
+    
+    // Create variable 'choice'
+    let choice;
+
+    // Switch between cases based on the random number
+    switch(randomNumber) {
+        case 0: // Rock
+            choice = "rock";
+            break;
+        case 1: // Paper
+            choice = "paper";
+            break;
+        case 2: // Scissors
+            choice = "scissors";
+            break;
+        default: // Invalid
+            console.error("The given random number was invalid!");
+            break;
+    }
+
+    console.log(choice);
+}
+
+getComputerChoice();
